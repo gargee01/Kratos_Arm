f14ed220ecccadedc33de5046eff13f2ad0b466f
Ace01o1 <iamace0101@gmail.com>
2024-11-26 21:41:30 +0530

    comments

--- groundwork/Manan/Gargee/Arduino codes/ld_arduino.ino
+++ groundwork/Manan/Gargee/Arduino codes/ld_arduino.ino
@@ -1,32 +1,40 @@
 #include <ros.h>
 #include <std_msgs/Float32MultiArray.h>
+#include <std_msgs/Int64.h>
 #include <ESP32Servo.h>
+#include "ArduinoHardware.h"
 ros::NodeHandle nh;
 int dir1=4;
-int pwm1=0;
+int pwm1=0; 
+
 int dir2=33;
 int pwm2=32;
-int dir3=26;
+
+int dir3=26; 
 int pwm3=25;
-int dir4=12;
+int dir4=12; //scissor lift
 int pwm4=14;
-int dir5=27;
+int dir5=27; // augre
 int pwm5=13;
-int dir6=5;
+int dir6=5; //magnetic mixer
 int pwm6=15;
 int en=18;
 int dirst=19;
 int step=23;
 float b[13];
 Servo myservo;
+int servo=11;
+int initial_position=0;
 
 void Callback( const std_msgs::Float32MultiArray& val){
   
-
+'['
   memcpy(b, val.data, sizeof(b));
 }
 
  ros::Subscriber<std_msgs::Float32MultiArray> Sub("publish", &Callback);
+
+
 
 void setup() {
   pinMode(dir1,OUTPUT);
@@ -45,8 +53,9 @@ void setup() {
   pinMode(dirst,OUTPUT);
   pinMode(step,OUTPUT);
   digitalWrite(en,HIGH);
-  myservo.attach(15);
- nh.getHardware()->setBaud(115200);
+  myservo.attach(servo);
+  myservo.write(0);
+  nh.getHardware()->setBaud(115200);
   nh.subscribe(Sub);
   nh.initNode();
 
@@ -61,8 +70,9 @@ void loop() {
   analogWrite(pwm6,0);
 
 
+
   if(b[0]==1)
-  {
+  { //pumps are unidirectional and on digital high
     digitalWrite(dir1, HIGH); 
  
     digitalWrite(pwm1,HIGH);  
@@ -82,11 +92,15 @@ void loop() {
     digitalWrite(pwm3,HIGH);  
   
   }
-  else if(b[2]==1 && b[3]>0.1)
+  
+  // the 4 buttons on the right. If rightmost is pressed then b[2]=1 (scissor lift) if uppermost is pressed then b[2] is 2 (augre) and if leftmost is pressed then b[2] is 3 (magnet)
+  // have to hold down the mentioned button and then move the left joystick up or down to change direction (button must be pressed while doing this)
+  else if(b[2]==1 && b[3]>0.1)  // scissor lift, magnetic spinner and augre are bi directional and analog
   {
     digitalWrite(dir4, HIGH); 
 
     analogWrite(pwm4,b[3]*255);  
+
  
   }
   else if(b[2]==1 && b[3]<-0.1)
@@ -94,6 +108,7 @@ void loop() {
     digitalWrite(dir4, LOW); 
 
     analogWrite(pwm4,(-1*b[3])*255);  
+
  
   }
   else if(b[2]==2 && b[3]>0.1)
@@ -101,38 +116,42 @@ void loop() {
     digitalWrite(dir5, HIGH); 
 
     analogWrite(pwm5,b[3]*255);  
- 
+
   }
   else if(b[2]==2 && b[3]<-0.1)
   {
     digitalWrite(dir5, LOW); 
 
     analogWrite(pwm5,(-1*b[3])*255);  
- 
+
   }
   else if(b[2]==3 && b[3]>0.1)
   {
     digitalWrite(dir6, HIGH); 
 
     analogWrite(pwm6,b[3]*255);  
- 
+
+  
   }
   else if(b[2]==3 && b[3]<-0.1)
   {
     digitalWrite(dir6, LOW); 
 
     analogWrite(pwm6,(-1*b[3])*255);  
+
+
  
   }
+  //b[4] is 1 when left trigger is pressed: enabled. b[4]=0 when right trigger is pressed. b[5] is 1 when the lowermost of the left 4 buttons is pressed. is -1 when lowermost of right 4 buttons is pressed.
   if(b[4]==1){
-    digitalWrite(en,LOW);
+    digitalWrite(en,LOW); // stepper is enabled 
     if(b[5]==1)
    {
     digitalWrite(dirst,HIGH);
-    digitalWrite(step, HIGH);
+    digitalWrite(step, HIGH);  
     delayMicroseconds(2000);
-    digitalWrite(step, LOW);
-    delayMicroseconds(2000);
+    digitalWrite(step, LOW) ;
+    delayMicroseconds(2000); // prev 5 lines make  one step in one direction
    }
     else if(b[5]==-1)
     {
@@ -140,38 +159,21 @@ void loop() {
       digitalWrite(step ,HIGH);
       delayMicroseconds(2000);
       digitalWrite(step, LOW);
-      delayMicroseconds(2000);
+      delayMicroseconds(2000); // prev 5 lines make  one step in other direction
 
     }
   }
-  int pos=myservo.read();
 
-   if (b[6]>0.1)
-    { 
-      
-      int i;
+  
 
 
-      for (i = pos; i <= pos+10; i += 1) 
-      { 
-         myservo.write(i);              // tell servo to go to position in variable 'pos'
-         delay(15);                       // waits 15 ms for the servo to reach the position
-     }
-    }
-   else if(b[6]<=-0.1)
-    {
-      int i;
-      for (i = pos; i >= pos-10; i -= 1) 
-      { 
-         myservo.write(i);              // tell servo to go to position in variable 'pos'
-         delay(15);                       // waits 15 ms for the servo to reach the position
-     }
-    }
+   
+  myservo.write(b[6]);
+  delay(100);
   
 
   
-  
-  // put your main code here, to run repeatedly:
+
   nh.spinOnce();
   delay(100);
 }
